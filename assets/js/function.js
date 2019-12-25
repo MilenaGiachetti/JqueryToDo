5ed62db76ad883e28187b2ac8df238e4a44a4113
MilenaGiachetti <giachettimilena@gmail.com>
2019-12-25 10:20:39 -0300

    list guardada en localStorage y aparece en la pantalla

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,10 +1,24 @@
+if(localStorage.listItems !== ''){
+    var localItems = new Array;
+    localItems = JSON.parse(localStorage.getItem('listItems'));
+    console.log(localItems);
+    for(i=0; i < localItems.length; i++){
+        console.log(localItems[i]);
+        $('ul').append('<li><p>'+localItems[i]+'</p><span class="trash btn"><i class="fas fa-trash-alt"></i></span><span class="check btn"><i class="fas fa-check"></i></span><span class="uncheck btn"><i class="fas fa-times"></i></span></li>'
+        );
+    }
+}
+
 //agregar nuevos li
+var listItems = [];
 $("input[type='text']").on('keypress', function(event){ 
     if (event.which === 13) {
         var texto = $("input[type='text']").val()
         $('ul').append('<li><p>'+texto+'</p><span class="trash btn"><i class="fas fa-trash-alt"></i></span><span class="check btn"><i class="fas fa-check"></i></span><span class="uncheck btn"><i class="fas fa-times"></i></span></li>'
         );
         $("input[type='text']").val('');
+        listItems.push(texto);
+        localStorage.setItem('listItems', JSON.stringify(listItems));
     }
 });
 $('.uncheck').toggleClass('hidden');
